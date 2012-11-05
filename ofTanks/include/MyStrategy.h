34aa752bed87588d45af6bda905845f9d723b06e
sk <sk@meercat.(none)>
2012-11-05 21:34:32 +0200

    up

--- ofTanks/include/MyStrategy.h
+++ ofTanks/include/MyStrategy.h
@@ -5,10 +5,38 @@
 
 #include "Strategy.h"
 
+enum MotionState {
+    STRAUGHT_AHEAD = 0,
+//    RANDOM_TURNING_LEFT,
+//    RANDOM_TURNING_RIGHT,
+    RANDOM_TURNING_LEFT_MAX,
+    RANDOM_TURNING_RIGHT_MAX,
+    STATES_NUMBER
+};
+
+enum PositionState {
+    IN_FIELD = 0,
+    ON_BORDER
+};
+
 class MyStrategy : public Strategy {
 public:
 	void Move(model::Tank self, model::World world, model::Move& move);
 	model::TankType SelectTank(int tank_index, int team_size);
+
+	float speed;
+	float direction;
+
+
+
+	float left;
+	float right;
+
+	int next_state_tick;
+
+
+	int state;
+
 };
 
 #endif
--- ofTanks/src/MyStrategy.cpp
+++ ofTanks/src/MyStrategy.cpp
@@ -11,9 +11,69 @@ using namespace model;
 
 void MyStrategy::Move(Tank self, World world, model::Move& move) {
 
-    move.set_left_track_power(-1.0);
-    move.set_right_track_power(1.0);
-    move.set_turret_turn(M_PI);
+    static bool init = false;
+
+    if (!init) {
+        init = true;
+        left = 1.0;
+        right = 1.0;
+        next_state_tick = 20;
+        state = STRAUGHT_AHEAD;
+    }
+
+    cout << "world tick " << world.tick() << " next_state_tick " << next_state_tick << endl;
+
+    if (world.tick() > next_state_tick) {
+
+        if (state == STRAUGHT_AHEAD) {
+            state = floor(ofRandom(1., STATES_NUMBER - 0.0001));
+            next_state_tick = world.tick() + 100;
+        } else {
+            state = STRAUGHT_AHEAD;
+            next_state_tick = world.tick() + 100;
+        }
+
+        cout << "state change " << state << " " << world.tick() << " "
+                << next_state_tick << endl;
+    }
+
+
+
+    if (state == STRAUGHT_AHEAD) {
+        right = left = 1.0;
+//    } else if (state == RANDOM_TURNING_LEFT) {
+//        left = -0.5 + ofRandom(-0.5, 1.);
+//        right = 0.7 + ofRandom(-0.1, 0.3);
+//    } else if (state == RANDOM_TURNING_RIGHT) {
+//        right = -0.5 + ofRandom(-0.5, 1.);
+//        left = 0.7 + ofRandom(-0.1, 0.3);
+    } else if (state == RANDOM_TURNING_LEFT_MAX) {
+        right = 1.0;
+        left = -1.0;
+    } else if (state == RANDOM_TURNING_RIGHT_MAX) {
+        right = -1.0;
+        left = 1.0;
+    }
+
+    // change motion state randomly
+    //cout << "tick " << world.tick() << endl;
+
+//    if (left > 10.0) left = 10.0;
+//    if (right > 10.0) right = 10.0;
+//
+//    if (left < -10.0) left = -10.0;
+//    if (right < -10.0) right = -10.0;
+//
+//    left += ofRandom(-0.01, 0.01);
+//    right += ofRandom(-0.01, 0.01);
+
+    move.set_left_track_power(left);
+    move.set_right_track_power(right);
+
+    //move.set_turret_turn(M_PI);
+
+    move.set_turret_turn(ofRandom(0, M_PI * 2));
+
     move.set_fire_type(PREMIUM_PREFERRED);
 
     std::vector<Tank> tanks = world.tanks();
@@ -26,13 +86,12 @@ void MyStrategy::Move(Tank self, World world, model::Move& move) {
         ofPushMatrix();
         ofPushStyle();
 
-        ofSetRectMode(OF_RECTMODE_CENTER);
+        ofSetRectMode (OF_RECTMODE_CENTER);
 
         ofTranslate(
                 ofPoint(tanks[i].x() * WORLD_SCALE,
                         tanks[i].y() * WORLD_SCALE));
         ofRotate(tanks[i].angle() * RAD_TO_DEG);
-
 
         ofRect(0, 0, tanks[i].width() * WORLD_SCALE,
                 tanks[i].height() * WORLD_SCALE);
@@ -43,11 +102,9 @@ void MyStrategy::Move(Tank self, World world, model::Move& move) {
         ofSetLineWidth(4.0f);
         ofLine(0, 0, 50, 0);
 
-
         ofPopStyle();
         ofPopMatrix();
     }
-
 
     std::vector<Shell> shells = world.shells();
 
@@ -56,14 +113,13 @@ void MyStrategy::Move(Tank self, World world, model::Move& move) {
         ofPushMatrix();
         ofPushStyle();
 
-        ofSetRectMode(OF_RECTMODE_CENTER);
+        ofSetRectMode (OF_RECTMODE_CENTER);
         ofSetColor(ofColor::red);
 
         ofTranslate(
                 ofPoint(shells[i].x() * WORLD_SCALE,
                         shells[i].y() * WORLD_SCALE));
         ofRotate(shells[i].angle() * RAD_TO_DEG);
-
 
         ofRect(0, 0, shells[i].width() * WORLD_SCALE,
                 shells[i].height() * WORLD_SCALE);
